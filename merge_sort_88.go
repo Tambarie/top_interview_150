94a808ac18f7f93d6d6ecda4b1b7513efd09f421
Gbaragbo Emmanuel Tambarie <gt.gbaragbo@gmail.com>
2023-08-08 22:25:17 +0100

    add merge sort_88

--- merge_sort_88.go
+++ merge_sort_88.go
@@ -36,3 +36,20 @@ func mergeSortApproachTwo(nums1 []int, m int, nums2 []int, n int) {
 	}
 
 }
+
+func mergeThree(nums1 []int, m int, nums2 []int, n int) {
+	var ptr1, ptr2, ptr3 int = m - 1, n - 1, m + n - 1
+	for ; ptr1 >= 0 && ptr2 >= 0; ptr3-- {
+		if nums2[ptr2] >= nums1[ptr1] {
+			nums1[ptr3] = nums2[ptr2]
+			ptr2--
+		} else {
+			nums1[ptr3] = nums1[ptr1]
+			ptr1--
+		}
+	}
+
+	if ptr2 >= 0 {
+		copy(nums1[:ptr3+1], nums2[:ptr2+1])
+	}
+}
